6f94bfc530b786ca518d8423672cade1f4755335
Melissa <mel.beee@deepteal.com>
2015-02-25 09:56:06 -0500

    Ajout des .jva

---
+++ Connexion.java
@@ -0,0 +1,23 @@
+// Terminateur.java
+// 
+// Fait par Melissa Boucher et Francis Thibodeau 
+// 20 Fevrier 2015
+import java.io.*;
+import java.net.*;
+
+public class Connexion
+{
+   public Socket unSocket_ = null;
+   
+	public Connexion(Socket unAutreSocket_)
+   {
+      unSocket_ = unAutreSocket_;
+   }
+   
+   public void Run()
+   {
+      String ligne = new String(); 
+   }
+}
+
+
---
+++ ServeurEcho.java
@@ -0,0 +1,15 @@
+// Terminateur.java
+// 
+// Fait par Melissa Boucher et Francis Thibodeau 
+// 20 Fevrier 2015
+import java.io.*;
+import java.net.*;
+
+public class ServeurEcho
+{
+	public static void main(String args[] )
+   {
+      ServeurEcho app = new ServeurEcho(); // instance de class 
+    	 
+   }
+}
