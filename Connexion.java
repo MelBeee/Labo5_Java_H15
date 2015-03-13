a643e84378c003914bcf86ed71977aac0cfb6a94
MelBeee <mel.beee@deepteal.com>
2015-03-12 22:40:14 -0400

    Blblbl

--- Connexion.java
+++ Connexion.java
@@ -1,23 +1,50 @@
-// Terminateur.java
-// 
-// Fait par Melissa Boucher et Francis Thibodeau 
-// 20 Fevrier 2015
-import java.io.*;
+import  java.io.*;
 import java.net.*;
 
-public class Connexion
+public class Connexion implements Runnable
 {
-   public Socket unSocket_ = null;
-   
-	public Connexion(Socket unAutreSocket_)
-   {
-      unSocket_ = unAutreSocket_;
-   }
-   
-   public void Run()
-   {
-      String ligne = new String(); 
-   }
-}
+	public Socket unSocket = null; 
+    public Connexion(Socket unSocketUtilise)
+    {
+		unSocket = unSocketUtilise;
+    }
 
+    public void run()
+    {
+		try
+		{
+			PrintWriter writer = new PrintWriter(new OutputStreamWriter(unSocket.getOutputStream()));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(unSocket.getInputStream()));
+			String uneLigne = new String();
+			
+			do
+			{
+				uneLigne = reader.readLine();
+				writer.println(uneLigne);
+				writer.flush();
+			}while(!uneLigne.isEmpty());
+			
+			writer.close();
+			reader.close();
+
+			System.out.println("Client deconnecte");
+		}
+		catch(IOException ioe)
+		{
+			System.err.println("Fermeture innattendue de session sans fermer la connexion");
+			System.exit(1);
+		}	
+		finally
+		{
+			try
+			{
+				unSocket.close();
+			}
+			catch(IOException ioe)
+			{ 
+				
+			}
+		}
+	}
+}
 
--- ServeurEcho.java
+++ ServeurEcho.java
@@ -1,15 +1,66 @@
-// Terminateur.java
-// 
-// Fait par Melissa Boucher et Francis Thibodeau 
-// 20 Fevrier 2015
 import java.io.*;
 import java.net.*;
 
-public class ServeurEcho
+public  class ServeurEcho
 {
-	public static void main(String args[] )
-   {
-      ServeurEcho app = new ServeurEcho(); // instance de class 
-    	 
-   }
+	public void lancerServeur(int port)
+	{
+		try
+		{
+			Socket unSocket = null;
+			ServerSocket socketServeur = null;
+			
+			socketServeur = new ServerSocket(port);
+			System.out.println("Serveur echo en attente d'une connexion.");
+			
+			Terminateur unTerminateur = new Terminateur();
+			Thread unDeTerminateur = new Thread(unTerminateur);
+			unDeTerminateur.setDaemon(true);
+			unDeTerminateur.start();
+			
+			while(unDeTerminateur.isAlive())
+			{
+				unSocket = socketServeur.accept();
+				System.out.println("Connexion du client.");
+				
+				Connexion uneConnexion = new Connexion(unSocket);
+				Thread unDeConnexion = new Thread(uneConnexion);
+				unDeConnexion.setDaemon(true);
+				unDeConnexion.start();
+			}	
+			
+			if(unSocket != null)
+			{
+				unSocket.close();
+				socketServeur.close();
+			}
+		
+			System.out.println("Deconnexion du client.");
+			System.exit(1);
+		}
+		catch(IOException ioe)
+		{
+			System.err.println(ioe);
+			System.exit(1);
+		}
+	}
+
+	public static void main(String args[]) throws IOException
+	{
+		ServeurEcho unBeauServeur = new ServeurEcho();
+		unBeauServeur.lancerServeur(7);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
