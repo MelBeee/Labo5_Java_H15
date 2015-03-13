f1914c7a6b9c9392981fb86f3877f4acb749d7d4
MelBeee <mel.beee@deepteal.com>
2015-03-12 23:40:42 -0400

    :end:

--- ServeurEcho.java
+++ ServeurEcho.java
@@ -10,24 +10,32 @@ public  class ServeurEcho
 			Socket unSocket = null;
 			ServerSocket socketServeur = null;
 			
-			socketServeur = new ServerSocket(port);
-			socketServeur.setSoTimeout(1000);
-			System.out.println("Serveur echo en attente d'une connexion.");
-			
 			Terminateur unTerminateur = new Terminateur();
 			Thread unDeTerminateur = new Thread(unTerminateur);
 			unDeTerminateur.setDaemon(true);
 			unDeTerminateur.start();
 			
+			socketServeur = new ServerSocket(port);
+			socketServeur.setSoTimeout(1000);
+			System.out.println("Serveur echo en attente d'une connexion.");
+			
+			
 			while(unDeTerminateur.isAlive())
 			{
-				unSocket = socketServeur.accept();
-				System.out.println("Connexion du client.");
-				
-				Connexion uneConnexion = new Connexion(unSocket);
-				Thread unDeConnexion = new Thread(uneConnexion);
-				unDeConnexion.setDaemon(true);
-				unDeConnexion.start();
+				try
+				{
+					unSocket = socketServeur.accept();
+					System.out.println("Connexion du client.");
+					
+					Connexion uneConnexion = new Connexion(unSocket);
+					Thread unDeConnexion = new Thread(uneConnexion);
+					unDeConnexion.setDaemon(true);
+					unDeConnexion.start();
+				}
+				catch (SocketTimeoutException  e)
+                {
+                  
+                }
 			}	
 			
 			if(unSocket != null)
@@ -36,7 +44,7 @@ public  class ServeurEcho
 				socketServeur.close();
 			}
 		
-			System.out.println("Deconnexion du client.");
+
 			System.exit(1);
 		}
 		catch(IOException ioe)
